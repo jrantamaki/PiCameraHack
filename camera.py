a4915fad737c06a26500048f3731d80cfecf7ab2
Jarkko Rantamaki <jarkko.rantamaki@iki.fi>
2021-09-07 19:33:04 +0300

    add command line parameter for target directory

--- camera.py
+++ camera.py
@@ -1,11 +1,13 @@
 from picamera import PiCamera
 from time import sleep
 from datetime import datetime
+from sys import argv
 
 camera = PiCamera()
 camera.resolution=(2560, 1440)
 timestamp = str(datetime.today().strftime('%Y-%m-%d-%H%M%S'))
-filename = str("/pics/") + timestamp + str(".png")
+filename = argv[1] + timestamp + str(".png")
+camera.framerate = 15
 camera.start_preview()
 sleep(5)
 camera.capture(filename)
@@ -17,5 +19,5 @@ camera.close()
 # mkdir /hacks/pics
 #
 #  crontab -e
-#  */5 * * * * /usr/bin/python /hacks/camera.py >>/hacks/cron.log 2>&1
+#  */5 * * * * /usr/bin/python /hacks/camera.py /hacks/pics >>/hacks/cron.log 2>&1
 # crontab -l
